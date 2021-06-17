cf4579d66a0b53e1435f7fe90ba90c01538d3eab
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-06-17 22:11:25 +0900

    add: insertIdolColor

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,8 @@
 import * as vscode from 'vscode'
+import { insertIdolColor } from './search'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "imas-color" is now active!')
-
-  let disposable = vscode.commands.registerCommand(
-    'imas-color.helloWorld',
-    async () => {
-      vscode.window.showInputBox()
-      const a = await vscode.window.showQuickPick(['abcd', 'abcd'])
-
-      console.log(a)
-      vscode.window.showInformationMessage('Hello World from imas-color!')
-    }
-  )
-
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(insertIdolColor)
 }
 
 export function deactivate() {}
